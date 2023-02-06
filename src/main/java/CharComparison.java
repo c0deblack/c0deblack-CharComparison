b73083199f63f8d2606991ef62db0f28834018d5
c0deblack <79178809+c0deblack@users.noreply.github.com>
2023-02-06 02:17:01 +0000

    Completed tests.

--- src/main/java/CharComparison.java
+++ src/main/java/CharComparison.java
@@ -1,3 +1,4 @@
+
 public class CharComparison {
     /**
      * Given an array of char, determine if the array A belongs to the left or to the right of char array B lexigraphically.
@@ -11,6 +12,17 @@ public class CharComparison {
      * @return -1 if A is less than B, 1 if A is greater than B, and 0 if the two arrays are identical.
      */
     public int compare(char[] a, char[] b){
-        return 0;
+        int range = a.length < b.length ? a.length 
+                    : a.length > b.length ? b.length
+                    : a.length;
+
+        for(int i = 0; i < range; i++)
+        {
+            if(a[i] < b[i]) return -1;
+            if(a[i] > b[i]) return 1;
+        }
+        if(a.length < b.length) return -1;
+        else if(a.length > b.length) return 1;
+        else return 0;
     }
 }
